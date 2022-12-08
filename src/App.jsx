23fc48ce071c30ef7dc1c7822627c37fa74376d5
Hluf Abebe <hlufabebe2015@gmail.com>
2022-12-08 08:30:25 -0800

    usereducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,7 @@
 import { useState } from "react";
-import Counter from "./Comp/Counter";
-
-function NameList() {
-  const [list, setlist] = useState(["Hluf", "Abebe", "Derbew"]);
-  let [name, setName] = useState("");
-
-  function addlistList() {
-    setlist([...list, name]);
-    setName("");
-  }
-
-  return (
-    <div>
-      <ul>
-        {list.map((list, index) => {
-          return <li key={index}>{list}</li>;
-        })}
-      </ul>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <button className="btn" onClick={addlistList}>
-        Add Name
-      </button>
-    </div>
-  );
-}
 
 function App() {
-  return (
-    <div className="App">
-      <Counter />
-      <NameList />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
-export default App;
+export default App; 

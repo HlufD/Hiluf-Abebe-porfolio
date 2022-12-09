dad63b91ebbd6ffc6d9957e56ea9e0e69ec45fab
Hluf Abebe <hlufabebe2015@gmail.com>
2022-12-08 21:33:18 -0800

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,81 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 
-function App() {
-  return <div className="App"></div>;
+function UserForm() {
+  const [state, dispach] = useReducer(
+    (state, action) => {
+      return { ...state, ...action };
+    },
+    {
+      first: "",
+      last: "",
+    }
+  );
+  return (
+    <div>
+      <input
+        type="text"
+        value={state.first}
+        onChange={(e) => dispach({ first: e.target.value })}
+      />
+      <input
+        type="text"
+        value={state.last}
+        onChange={(e) => dispach({ last: e.target.value })}
+      />
+      <p>{state.first}</p>
+      <p>{state.last}</p>
+    </div>
+  );
 }
 
-export default App; 
+function NameList() {
+  const [state, dispach] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "SET_NAME":
+          return { ...state, name: action.payload };
+        case "ADD_NAME":
+          return {
+            ...state,
+            names: [...state.names, action.payload],
+            name: "",
+          };
+      }
+    },
+    {
+      names: [],
+      name: "",
+    }
+  );
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        value={state.name}
+        onChange={(e) => dispach({ type: "SET_NAME", payload: e.target.value })}
+      />
+      <button
+        onClick={() => dispach({ type: "ADD_NAME", payload: state.name })}
+      >
+        Add Name
+      </button>
+      {state.names.length === 0 ? (
+        <p>Empty Name List</p>
+      ) : (
+        state.names.map((name, index) => <p key={index}>{name}</p>)
+      )}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <UserForm />
+      <NameList />
+    </>
+  );
+}
+
+export default App;
